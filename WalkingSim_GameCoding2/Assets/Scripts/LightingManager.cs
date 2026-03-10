67c1bd1a876fbb4f499cf4562f0d65744425fb8f
Damien <dbrow365@pratt.edu>
2026-03-10 14:11:00 -0400

    reworked lighting system

---
+++ WalkingSim_GameCoding2/Assets/Scripts/LightingManager.cs
@@ -0,0 +1,63 @@
+using UnityEngine;
+
+
+[ExecuteAlways]
+public class LightingManager : MonoBehaviour
+{
+    [SerializeField] private Light directionalLight;
+    [SerializeField] private LightingPreset preset;
+
+    [SerializeField, Range(0, 24)] private float timeOfDay;
+
+
+    private void Update()
+    {
+        if (preset == null)
+            return;
+
+        if(Application.isPlaying)
+        {
+            timeOfDay += Time.deltaTime;
+            timeOfDay %= 24; //clamped
+            UpdateLighting(timeOfDay / 24f);
+        }
+        else
+        {
+            UpdateLighting(timeOfDay / 24f);
+        }
+    }
+
+    private void UpdateLighting(float timePercent)
+    {
+        RenderSettings.ambientLight = preset.AmbientColor.Evaluate(timePercent);
+        RenderSettings.fogColor = preset.FogColor.Evaluate(timePercent);
+
+        if(directionalLight != null )
+        {
+            directionalLight.color = preset.DirectionalColor.Evaluate(timePercent);
+            directionalLight.transform.localRotation = Quaternion.Euler(new Vector3((timePercent * 360f) - 90f, 170f, 0));
+        }
+    }
+    private void OnValidate()
+    {
+        if (directionalLight != null)
+            return;
+
+        if (RenderSettings.sun != null)
+        {
+            directionalLight = RenderSettings.sun;
+        }
+        else
+        {
+            Light[] lights = FindObjectsOfType<Light>();
+            foreach (Light light in lights)
+            {
+                if (light.type == LightType.Directional)
+                {
+                    directionalLight = light;
+                    return;
+                }
+            }
+        }
+    }
+}
